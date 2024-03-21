d1276040fb593850d13d593e76261bc51d4a6dda
nguyen2192004 <phamgianguyen.qn@gmail.com>
2024-03-21 16:28:21 +0700

    enclose

--- lib/SignInScreen.dart
+++ lib/SignInScreen.dart
@@ -7,6 +7,8 @@ class SignInScreen extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             body: Container(
+      width: double.infinity,
+      height: double.infinity,
       decoration: const BoxDecoration(
         image: DecorationImage(
           image: AssetImage('assets/images/background.jpg'),
@@ -14,69 +16,78 @@ class SignInScreen extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(height: 200),
-              Center(
-                child: Container(
-                  width: 500,
-                  child: const TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Enter your username'),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: Container(
-                  width: 500,
-                  child: const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Enter your password'),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: 500,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.red,
-                ),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text('Login',
-                      style: TextStyle(color: Colors.white)),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Don\'t have an account?',
-                        style: TextStyle(color: Colors.white)),
-                    const SizedBox(width: 10),
-                    Container(
-                      width: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.red,
+        child: Container(
+          width: 700,
+          height: 700,
+          decoration: const BoxDecoration(
+            color: Colors.black54,
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  const SizedBox(height: 200),
+                  Center(
+                    child: Container(
+                      width: 500,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter your username'),
                       ),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: const Text('Sign up'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: Container(
+                      width: 500,
+                      child: const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter your password'),
                       ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: 500,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.red,
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text('Login',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Don\'t have an account?',
+                            style: TextStyle(color: Colors.white)),
+                        const SizedBox(width: 10),
+                        Container(
+                          width: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.red,
+                          ),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text('Sign up'),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 //import signinScreen.dart;
 import 'SignInScreen.dart';
+import 'SignUpScreen.dart';
 
 void main() {
-  runApp(const SignInScreen());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: (settings) {
+        // Use switch to provide routes for different route names
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (_) => SignInScreen());
+          case '/signup':
+            return MaterialPageRoute(builder: (_) => SignUpScreen());
+          // can add more cases here for more screens
+          default:
+            return MaterialPageRoute(builder: (_) => SignInScreen());
+        }
+      },
+      initialRoute: '/',
+    );
+  }
 }

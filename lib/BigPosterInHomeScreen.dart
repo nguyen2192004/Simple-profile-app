7b9798b40e025c7cd81e54a3dc9627d4e561b114
nguyen2192004 <phamgianguyen.qn@gmail.com>
2024-03-22 15:09:58 +0700

    Update BigPosterInHomeScreen.dart

--- lib/BigPosterInHomeScreen.dart
+++ lib/BigPosterInHomeScreen.dart
@@ -4,15 +4,50 @@ class BigPosterInHomeScreen extends StatelessWidget {
   List<Image> posters = [];
   BigPosterInHomeScreen({Key? key}) : super(key: key);
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/EnolaHolmes2.jpg'),
-          fit: BoxFit.contain,
+    return ListView(children: [
+      Container(
+        height: 500,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/EnolaHolmes2.jpg'),
+            fit: BoxFit.contain,
+          ),
         ),
       ),
-    );
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          TextButton(
+              onPressed: () {},
+              child: const Icon(Icons.add, color: Colors.white)),
+          Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.red,
+            ),
+            child: TextButton(
+                onPressed: () {},
+                child: const Row(
+                  children: [
+                    Icon(Icons.play_arrow, color: Colors.white),
+                    Text('Play', style: TextStyle(color: Colors.white)),
+                  ],
+                )),
+          ),
+          //a circle button
+          Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.info, color: Colors.black),
+            ),
+          )
+        ],
+      )
+    ]);
   }
 }
